d421e7a2ce961baa1d84a5a243d10d42a4dfae1d
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-09-30 12:24:10 +0200

    Create DeleteMenuItem

--- API/Controllers/MenuItemController.cs
+++ API/Controllers/MenuItemController.cs
@@ -189,4 +189,53 @@ public class MenuItemController : Controller
 
         return BadRequest(_response);
     }
+
+    [HttpDelete]
+    public async Task<ActionResult<ApiResponse>> DeleteMenuItem(int id)
+    {
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                if (id == 0)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    return BadRequest(_response);
+                }
+
+                MenuItem? menuItemFromDb = await _db.MenuItems.FirstOrDefaultAsync(u => u.Id == id);
+
+                if (menuItemFromDb == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    return NotFound(_response);
+                }
+
+                var filePath_OldFile = Path.Combine(_environment.WebRootPath, menuItemFromDb.Image);
+                if (System.IO.File.Exists(filePath_OldFile))
+                {
+                    System.IO.File.Delete(filePath_OldFile);
+                }
+
+                _db.MenuItems.Remove(menuItemFromDb);
+                await _db.SaveChangesAsync();
+
+                _response.StatusCode = HttpStatusCode.NoContent;
+                return Ok(_response);
+            }
+            else
+            {
+                _response.IsSuccess = false;
+            }
+        }
+        catch (Exception ex)
+        {
+            _response.IsSuccess = false;
+            _response.ErrorMessages = [ex.ToString()];
+        }
+
+        return BadRequest(_response);
+    }
 }
