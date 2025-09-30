4f3928a3dce7318c184027f1a47f6784d9fdcc8b
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-09-30 12:14:47 +0200

    Create CreateMenuItem

--- API/Controllers/MenuItemController.cs
+++ API/Controllers/MenuItemController.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using API.Data;
+using API.DTOs;
 using API.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,11 +12,13 @@ public class MenuItemController : Controller
 {
     private readonly ApplicationDbContext _db;
     private readonly ApiResponse _response;
+    private readonly IWebHostEnvironment _environment;
 
-    public MenuItemController(ApplicationDbContext db)
+    public MenuItemController(ApplicationDbContext db, IWebHostEnvironment environment)
     {
         _db = db;
         _response = new ApiResponse();
+        _environment = environment;
     }
 
     [HttpGet]
@@ -43,5 +46,70 @@ public class MenuItemController : Controller
         _response.Result = menuItem;
         _response.StatusCode = HttpStatusCode.OK;
         return Ok(_response);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ApiResponse>> CreateMenuItem([FromForm] MenuItemCreateDTO menuItemCreateDTO)
+    {
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                if (menuItemCreateDTO.File == null || menuItemCreateDTO.File.Length == 0)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.ErrorMessages = ["File is required"];
+                    return BadRequest(_response);
+                }
+
+                var imagesPath = Path.Combine(_environment.WebRootPath, "images");
+                if (!Directory.Exists(imagesPath))
+                {
+                    Directory.CreateDirectory(imagesPath);
+                }
+                
+                var filePath = Path.Combine(imagesPath, menuItemCreateDTO.File.FileName);
+                if (System.IO.File.Exists(filePath))
+                {
+                    System.IO.File.Delete(filePath);
+                }
+
+                // Upload image
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await menuItemCreateDTO.File.CopyToAsync(stream);
+                }
+
+                MenuItem menuItem = new()
+                {
+                    Name = menuItemCreateDTO.Name,
+                    Description = menuItemCreateDTO.Description,
+                    Price = menuItemCreateDTO.Price,
+                    Category = menuItemCreateDTO.Category,
+                    SpecialTag = menuItemCreateDTO.SpecialTag,
+                    Image = "images/" + menuItemCreateDTO.File.FileName
+                };
+
+                _db.MenuItems.Add(menuItem);
+                await _db.SaveChangesAsync();
+
+                _response.Result = menuItemCreateDTO;
+                _response.StatusCode = HttpStatusCode.Created;
+                return CreatedAtRoute("GetMenuItem", new { id = menuItem.Id }, _response);
+
+            }
+            else
+            {
+                _response.IsSuccess = false;
+            }
+        }
+        catch (Exception ex)
+        {
+            _response.IsSuccess = false;
+            _response.ErrorMessages = [ex.ToString()];
+        }
+
+        return BadRequest(_response);
     }
 }
