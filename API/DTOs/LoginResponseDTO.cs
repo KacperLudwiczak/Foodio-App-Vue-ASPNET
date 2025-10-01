51b9c13b96e0721a559fc70966a76623f2e1961b
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-10-01 14:51:23 +0200

    Create LoginResponseDTO

---
+++ API/DTOs/LoginResponseDTO.cs
@@ -0,0 +1,8 @@
+namespace API.DTOs;
+
+public class LoginResponseDTO
+{
+    public string Email { get; set; } = string.Empty;
+    public string Token { get; set; } = string.Empty;
+    public string Role { get; set; }= string.Empty;
+}
