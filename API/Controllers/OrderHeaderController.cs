9b6ddf78c941c3d34407c17a03b234daf3b40d50
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-10-02 11:59:45 +0200

    Create OrderHeaderController

---
+++ API/Controllers/OrderHeaderController.cs
@@ -0,0 +1,20 @@
+using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[Authorize]
+[ApiController]
+public class OrderHeaderController : Controller
+{
+    private readonly ApplicationDbContext _db;
+    private readonly ApiResponse _response;
+    public OrderHeaderController(ApplicationDbContext db)
+    {
+        _db = db;
+        _response = new ApiResponse();
+    }
+}
