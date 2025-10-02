bd718921348448e8c0b9d0769bcaa1670887d12a
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-10-02 12:26:31 +0200

    Create UpdateOrder for OrderDetailsController

--- API/Controllers/OrderDetailsController.cs
+++ API/Controllers/OrderDetailsController.cs
@@ -1,4 +1,6 @@
+using System.Net;
 using API.Data;
+using API.DTOs;
 using API.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -16,5 +18,55 @@ public class OrderDetailsController : Controller
     {
         _db = db;
         _response = new ApiResponse();
+    }
+
+    [HttpPut("{orderDetailsId:int}")]
+    public ActionResult<ApiResponse> UpdateOrder(int orderDetailsId, [FromBody] OrderDetailsUpdateDTO orderDetailsDTO)
+    {
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                if (orderDetailsId != orderDetailsDTO.OrderDetailId)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.ErrorMessages.Add("Invalid Id");
+                    return BadRequest(_response);
+                }
+
+                OrderDetail? orderDetailsFromDb = _db.OrderDetails
+                    .FirstOrDefault(x => x.OrderDetailId == orderDetailsId);
+
+                if (orderDetailsFromDb == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    _response.ErrorMessages.Add("Order not found");
+                    return NotFound(_response);
+                }
+                orderDetailsFromDb.Rating = orderDetailsDTO.Rating;
+                _db.SaveChanges();
+                _response.StatusCode = HttpStatusCode.NoContent;
+                return Ok(_response);
+            }
+            else
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.ErrorMessages = ModelState.Values
+                    .SelectMany(x => x.Errors)
+                    .Select(x => x.ErrorMessage)
+                    .ToList();
+                return BadRequest(_response);
+            }
+        }
+        catch (Exception ex)
+        {
+            _response.IsSuccess = false;
+            _response.StatusCode = HttpStatusCode.InternalServerError;
+            _response.ErrorMessages.Add(ex.Message);
+            return StatusCode((int)HttpStatusCode.InternalServerError, _response);
+        }
     }
 }
