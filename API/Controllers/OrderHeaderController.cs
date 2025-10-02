5ac1ff3790d90b5bbc5bbda389103c7764513857
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-10-02 12:10:34 +0200

    Create GetOrders

--- API/Controllers/OrderHeaderController.cs
+++ API/Controllers/OrderHeaderController.cs
@@ -1,7 +1,10 @@
+using System.Net;
 using API.Data;
 using API.Models;
+using API.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
@@ -16,5 +19,33 @@ public class OrderHeaderController : Controller
     {
         _db = db;
         _response = new ApiResponse();
+    }
+
+    [HttpGet]
+    public ActionResult<ApiResponse> GetOrders(string userId = "")
+    {
+        IEnumerable<OrderHeader> orderHeaderList = _db.OrderHeaders
+            .Include(x => x.OrderDetails)
+            .ThenInclude(x => x.MenuItem)
+            .OrderByDescending(x => x.OrderHeaderId);
+
+        if (!string.IsNullOrEmpty(userId))
+        {
+            orderHeaderList = orderHeaderList.Where(x => x.ApplicationUserId == userId);
+            _response.Result = orderHeaderList;
+        }
+        else
+        {
+            if (User.IsInRole(StaticDetails.Role_Admin))
+            {
+                _response.Result = orderHeaderList;
+            }
+            else
+            {
+                _response.Result = new List<OrderHeader>();
+            }
+        }           
+        _response.StatusCode = HttpStatusCode.OK;
+        return Ok(_response);
     }
 }
