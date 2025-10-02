59ac685ebde173e283a5698416ce85853371fd58
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-10-02 11:48:40 +0200

    Create OrderDetailsCreateDTO

---
+++ API/DTOs/OrderDetailsCreateDTO.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
+
+public class OrderDetailsCreateDTO
+{
+    [Required]
+    public int MenuItemId { get; set; }
+    [Required]
+    public int Quantity { get; set; }
+    [Required]
+    public string ItemName { get; set; } = string.Empty;
+    [Required]
+    public double Price { get; set; }
+}
