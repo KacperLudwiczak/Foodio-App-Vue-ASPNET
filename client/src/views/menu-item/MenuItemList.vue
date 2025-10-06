1cfb3ccb118b7dd810820a05efd1143e9ee0de51
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-10-06 14:16:22 +0200

    Add Service to Menu Item List

--- client/src/views/menu-item/MenuItemList.vue
+++ client/src/views/menu-item/MenuItemList.vue
@@ -1,5 +1,27 @@
 <template>
     <div class="container d-flex justify-content-center align-items-center no-access-container">
-        Menu List
+        Menu Items: {{ menuItems }}
     </div>
 </template>
+
+<script setup>
+import menuitemService from '@/services/menuItemService'
+import { ref, onMounted } from 'vue'
+
+const menuItems = ref([])
+const loading = ref(false)
+
+const fetchMenuItems = async () => {
+    loading.value = true
+    try {
+        menuItems.value = await menuitemService.getMenuItems()
+        console.log(menuItems.value)
+    } catch (error) {
+        console.log('Error fetch menu items:', error)
+    } finally {
+        loading.value = false
+    }
+}
+
+onMounted(fetchMenuItems)
+</script>
