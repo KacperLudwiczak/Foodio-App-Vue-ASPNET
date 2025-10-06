1e2a0a1b19fcb3457f232eb170422bee9bacd0d8
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-10-06 11:43:47 +0200

    Create Footer

---
+++ client/src/components/layout/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <div
+        class="footer text-center mx-auto mt-4 p-3 bg-opacity-10 text-success bg-success rounded-3"
+        >
+        &copy; Kacper <i class="bi bi-heart-fill"></i>
+        </div>
+    </div>
+</template>
