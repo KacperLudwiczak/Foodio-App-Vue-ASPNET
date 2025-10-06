3cdaab9b95fea576c86431d31678eefc591a985e
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-10-06 14:15:29 +0200

    Change Path

--- client/src/constants/config.js
+++ client/src/constants/config.js
@@ -1,2 +1,2 @@
-export const CONFIG_API_URL = 'https://localhost:5022/api'
-export const CONFIG_IMAGE_URL = 'https://localhost:5022/'
+export const CONFIG_API_URL = 'http://localhost:5022/api'
+export const CONFIG_IMAGE_URL = 'http://localhost:5022/'
