5ceaff5681f9bbbc5f6bfcdf6aa96d608ca34b5a
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-10-07 15:37:17 +0200

    Create useAlert

---
+++ client/src/hooks/useAlert.js
@@ -0,0 +1,39 @@
+import Swal from 'sweetalert2'
+
+export function useAlert() {
+    const showAlert = async (options) => {
+        Swal.fire(options)
+    }
+
+    const showSuccess = async (message) => {
+        return await showAlert({
+        icon: 'success',
+        title: message,
+        showConfirmButton: false,
+        timer: 1500,
+        })
+    }
+
+    const showError = async (message) => {
+        return await showAlert({
+        icon: 'error',
+        title: message,
+        showConfirmButton: false,
+        timer: 1500,
+        })
+    }
+
+    const showConfirm = async (message) => {
+        return await showAlert({
+        title: 'Are you sure?',
+        text: message,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3eb489',
+        cancelButtonColor: '#ff8800',
+        confirmButtonText: 'Yes, delete it!',
+        })
+    }
+
+    return { showError, showSuccess, showConfirm }
+}
